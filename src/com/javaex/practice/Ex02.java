c7ff7f0f6471868e0674f0e2a0d6502046b4f4ef
ahah2025 <adghty61>
2025-04-21 17:24:10 +0900

    과제

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,55 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[ ]args) { //char,int,String
+		//(2)	char 
+		char grade = 'a';
+		
+		switch(grade) {
+		case 'A':
+		case 'a':
+			System.out.println("우수 회원입니다."); //우수 회원입니다.
+			break;
+		case 'B':
+		case 'b':
+			System.out.println("일반 회원입니다.");
+			break;	
+		default:
+			System.out.println("손님입니다.");
+		}
+		
+		//(5) int
+	/*	int time = (int) (Math.random() * 4) + 9;
+		System.out.println("[현재시간: " + time + " 시]") ;
+		
+		switch(time) { //break 문이 없는 case
+		case 8:
+			System.out.println("출근합니다.");
+			
+		case 9:
+			System.out.println("회의를 합니다.");
+			
+		case 10:
+			System.out.println("업무를 봅니다.");
+			
+		default:
+			System.out.println("외근을 나갑니다.");
+		} */
+		
+		//(9)	String
+		/*	String position = "과장";
+			
+			switch(position) {
+			case "부장":
+				System.out.println("700만원");
+				break;
+			case "과장":
+				System.out.println("500만원"); //500만원
+			 	break;
+			default:
+				System.out.println("300만원");
+			}
+			*/
+	}
+}
---
+++ src/com/javaex/practice/Ex04.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		int x = 0;
+		
+		
+		// x가 0과 같으면
+		if( x == 0 ) { // ' = ' 부등호를 2개 기재해야 함
+			System.out.println("x는 0이다.");  //결과값 x는 0이다.
+		}
+
+	}
+
+}
