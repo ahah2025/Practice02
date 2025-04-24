3bc55ce1db576f49f3cfad698e96a6f41512643d
ahah2025 <adghty61>
2025-04-24 11:31:01 +0900

    3rd

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -6,17 +6,11 @@ public class Ex09 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+			System.out.println("태어난 년도를 입력해 주세요");
+			System.out.println("년도: ");
+			
+			
 		
-		System.out.println("태어난 년도를 입력해 주세요");
-		System.out.print("년도: ");
-		int year = sc.nextInt();
-		
-		System.out.print(year%4 - year%3);
-		System.out.println( year +" 살 무료예방접종 대상자 입니다");
-		
-		sc.close();
-
 	}
 }
 
